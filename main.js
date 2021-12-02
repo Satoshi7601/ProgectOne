2fd9e2f944130b1689a45bece52c9111a524e5ae
Raphael Bellagama <gabiel4k12@gmail.com>
2021-12-02 11:23:32 -0300

    atualiza

---
+++ main.js
@@ -0,0 +1,19 @@
+function consultacep(){
+    
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws" +cep+"/json/";
+    console.log(url);
+
+    $.ajax({
+        url: url,type:"GET",
+        success: function(response){
+            console.log(response);
+
+            $("#logradouro").html(response.logradouro);
+        }
+
+
+
+    })
+
+}
